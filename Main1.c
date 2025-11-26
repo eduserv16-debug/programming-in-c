e044ec18e3a646a0e838103bd15032d5aa0e208a
Raj lakshmi <eduserv.16@gmail.com>
2025-11-26 22:36:23 +0530

    Simplify input process for sum calculation
    Removed duplicate input section and simplified the program to only ask for input once.

--- Main1.c
+++ Main1.c
@@ -5,15 +5,12 @@
 int main() {
     int num1, num2, sum;
 
-    printf("Input 1:\n");
+    printf("Input :\n");
     scanf("%d %d", &num1, &num2);
     sum = num1 + num2;
     printf("Output 1:\nSum = %d\n\n", sum);
 
-    printf("Input 2:\n");
-    scanf("%d %d", &num1, &num2);
-    sum = num1 + num2;
-    printf("Output 2:\nSum = %d\n", sum);
 
     return 0;
 }
+
