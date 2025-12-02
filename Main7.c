694dd63e7e32b24f0e51324b472091218dcff45a
Raj lakshmi <eduserv.16@gmail.com>
2025-12-02 23:27:01 +0530

    Update Main7.c

--- Main7.c
+++ Main7.c
@@ -5,21 +5,19 @@
 int main() {
     int a, b;
 
-    // First input
-    printf("Input 1:\n");
-    scanf("%d %d", &a, &b);
-    a = a + b;
-    b = a - b;
-    a = a - b;
-    printf("Output 1:\nAfter swap: %d %d\n\n", a, b);
+    printf("Enter first number: ");
+    scanf("%d", &a);
 
-    // Second input
-    printf("Input 2:\n");
-    scanf("%d %d", &a, &b);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
     a = a + b;
     b = a - b;
     a = a - b;
-    printf("Output 2:\nAfter swap: %d %d\n", a, b);
+    
+    printf("After swapping:\n");
+    printf("First number = %d\n", a);
+    printf("Second number = %d\n", b);
 
     return 0;
 }
